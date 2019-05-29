8e4357acd4185e185f7a91b43ce1cf324123575c
aldion amirrul <aldionamirrul@gmail.com>
2019-05-29 13:38:43 +0700

    add UI create laboratory

--- resources/views/administrator/laboratory/index.blade.php
+++ resources/views/administrator/laboratory/index.blade.php
@@ -31,12 +31,11 @@
                     <div class="d-flex">
                         <div class="breadcrumb">
 							<a href="{{ url('') }}" class="breadcrumb-item"><i class="icon-home2 mr-2"></i> {{ config('app.name') }}</a>
-							<span class="breadcrumb-item active">Laboratory</span>
+							<span class="breadcrumb-item active">Laboratories</span>
 						</div>
 
                         <a href="#" class="header-elements-toggle text-default d-md-none"><i class="icon-more"></i></a>
                     </div>
-
 
 					<div class="header-elements d-none">
 						<div class="d-flex justify-content-center">
@@ -49,8 +48,42 @@
 
             <!-- Content area -->
             <div class="content">            
-                {{-- Write Your Content Here --}}
-                Hello World ;))
+                
+                <!-- Inner container -->
+                <div class="d-flex align-items-start flex-column flex-md-row">
+
+                    {{-- Do Foreach Here --}}
+                    <div class="col-md-4">
+                        <div class="card">
+                            <div class="card-header bg-transparent header-elements-inline">
+                                <h6 class="card-title">Desain Komputer</h6>                           
+                            </div>
+        
+                            <div class="card-img-actions">
+                                <img class="img-fluid" src="{{ url('pictures/laboratory_logo/Daskom.jpg') }}" alt="">
+                                <div class="card-img-actions-overlay">                                    
+                                    <a href="#" data-toggle="modal" data-target="#modal" class="btn btn-outline bg-white text-white border-white border-2 ml-2">
+                                        Change
+                                    </a>
+                                    <a href="#"  class="btn btn-outline bg-white text-white border-white border-2 ml-2">
+                                        Delete
+                                    </a>
+                                    <a href="#"  class="btn btn-outline bg-white text-white border-white border-2 ml-2">
+                                        Lab Detail
+                                    </a>
+                                </div>
+                            </div>                                    
+        
+                            <div class="card-footer">
+                                <p align="center">Graha Wiyata lt.2 </p>
+                            </div>
+                        </div>                        
+                    </div>
+                    {{-- End Foreach Here --}}
+
+                </div>
+                <!-- /inner container -->
+                
             </div>
             <!-- /content area -->
             
